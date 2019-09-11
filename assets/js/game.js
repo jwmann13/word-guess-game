d3128bbf98991c96af095979316db9b7be289787
Jeffrey Mann <jwmann22@gmail.com>
2019-09-10 22:18:56 -0400

    added reset function and commenting

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,64 +1,96 @@
 $(document).ready(function () {
     // Initialize list of words, pick random word from list
-    const wordList = ["horse", "apple", "bushes", "comb", "police"];
-    const targetWord = wordList[Math.floor(Math.random() * wordList.length)];
+    const wordList = ["orange", "apple", "banana", "grape", "pineapple", "kumquat", "kiwi", "tangerine", "pear", "fig", "tomato", "pomegranate", "apricot", "blueberry", "strawberry"];
+    let targetWord = wordList[Math.floor(Math.random() * wordList.length)];
     const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
     // Grab div to hold the user interfacing text
     const blankSpace = $(".blanks");
     const guessIndicator = $(".guess-number");
 
+    // Set up dynamic variables to hold background data
     let blankStr = "";
     let guessCounter = 5;
-    let wrongGuesses = []
+    let wrongGuesses = [];
 
+    // fill blank string with dashes(as place holders) and spaces(for legibility);
     for (let i = 0; i < targetWord.length; i++) {
         blankStr = blankStr + "_ ";
     }
-    for (let i = 0; i < letters.length; i++){
+    //  create letter indicators to html;
+    for (let i = 0; i < letters.length; i++) {
         var letterDisplay = $("<div>");
-        letterDisplay.addClass("lead col-2");
+        letterDisplay.addClass("lead col-2 character");
         letterDisplay.attr("data-letter", letters[i], "style", "font-size: 32px")
         letterDisplay.text(letters[i]);
         $(".letters").append(letterDisplay);
     }
+    // show those on page;
     blankSpace.text(blankStr);
     guessIndicator.text("You have " + guessCounter + " more wrong guesses");
 
+    // main user interaction function
     $(document).keyup(function (event) {
         let userGuess = event.key;
 
-        if (guessCounter > 1) {
-            if (targetWord.indexOf(userGuess) != -1) {
-                let temp = blankStr.replace(/\s/g, '');
-                blankStr = "";
-                for (let i = 0; i < targetWord.length; i++) {
-                    if (targetWord[i] === userGuess) {
-                        blankStr = blankStr + userGuess + " ";
-                    } else if (targetWord[i] != userGuess && temp[i] === "_") {
-                        blankStr = blankStr + "_ ";
-                    } else {
-                        blankStr = blankStr + temp[i] + " ";
+        // handles non letter key presses
+        if (letters.includes(userGuess.toUpperCase())) {
+            // handles play vs loss state
+            if (guessCounter > 1) {
+                if (targetWord.indexOf(userGuess) != -1) {
+                    // hold temporary string of correctly guessed letters and dashes with no spaces
+                    let temp = blankStr.replace(/\s/g, '');
+                    // reset blank string
+                    blankStr = "";
+                    // refill blank string with dashes for un-guessed letters(dahses) and correctly guessed letters(userGuess)
+                    for (let i = 0; i < targetWord.length; i++) {
+                        // guess is correct, write to string
+                        if (targetWord[i] === userGuess) {
+                            blankStr = blankStr + userGuess + " ";
+                            // guess is incorrect, write a dash
+                        } else if (targetWord[i] != userGuess && temp[i] === "_") {
+                            blankStr = blankStr + "_ ";
+                            // letter is already in string, rewrite to string
+                        } else {
+                            blankStr = blankStr + temp[i] + " ";
+                        }
                     }
-                }
-                $('[data-letter=' + userGuess.toUpperCase() + ']').css({'color': 'green', 'font-weight': 'bold'});
-                blankSpace.text(blankStr);
-                if(targetWord == blankStr.replace(/\s/g, '')){
-                    alert("You Win!");
+                    // correctly guessed letters made green and bold in alphabet interface
+                    $('[data-letter=' + userGuess.toUpperCase() + ']').css({
+                        'color': 'green',
+                        'font-weight': 'bold',
+                    });
+                    // write resulting string to page
+                    blankSpace.text(blankStr);
+                    // win state
+                    if (targetWord == blankStr.replace(/\s/g, '')) {
+                        alert("You Win!");
+                        reset();
+                    }
+                } else {
+                    guessCounterDown(userGuess);
+                    // incorrectly guessed letters made red and struckthrough in alphabet interface
+                    $('[data-letter=' + userGuess.toUpperCase() + ']').css({
+                        'color': 'red',
+                        'text-decoration': 'line-through'
+                    });
+                    guessIndicator.text("You have " + guessCounter + " more wrong guesses");
                 }
             } else {
+                // engages loss state
                 guessCounterDown(userGuess);
-                console.log("wrong", guessCounter);
-                $('[data-letter=' + userGuess.toUpperCase() + ']').css({'color': 'red', 'text-decoration': 'line-through'});
+                $('[data-letter=' + userGuess.toUpperCase() + ']').css({
+                    'color': 'red',
+                    'text-decoration': 'line-through'
+                });
                 guessIndicator.text("You have " + guessCounter + " more wrong guesses");
+                alert("Ya lost!");
+                reset();
             }
-        } else {
-            guessCounterDown(userGuess);
-            guessIndicator.text("You have " + guessCounter + " more wrong guesses");
-            alert("Ya lost!");
         }
     });
 
+    // function to count down wrong guesses
     function guessCounterDown(guess) {
         if (!wrongGuesses.includes(guess)) {
             wrongGuesses.push(guess);
@@ -66,6 +98,19 @@ $(document).ready(function () {
                 guessCounter -= 1;
             }
         }
+    }
+
+    function reset() {
+        targetWord = wordList[Math.floor(Math.random() * wordList.length)];
+        blankStr = "";
+        guessCounter = 5;
+        wrongGuesses = [];
+        for (let i = 0; i < targetWord.length; i++) {
+            blankStr = blankStr + "_ ";
+        }
+        $('.character').removeAttr('style')
+        blankSpace.text(blankStr);
+        guessIndicator.text("You have " + guessCounter + " more wrong guesses");
     }
 });
 
